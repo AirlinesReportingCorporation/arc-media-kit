337d5abdd73638e755b650d32c3e42d8cff2a200
Ian Fajardo <ianfajardozx@gmail.com>
2022-09-16 11:41:09 -0400

    update with correct data

--- src/app.jsx
+++ src/app.jsx
@@ -22,15 +22,20 @@ class App extends Component {
                 <div className="arc-media-kit-jumbo-inner">
                   <h1>Air Travel Insight for the Media</h1>
                   <p className="arc-media-kit-subtitle">
-                    ARC settled more than $97.4 billion worth of airfare
-                    transactions in 2019 between airlines and travel agencies,
-                    including major online agencies such as Expedia. The data
-                    acquired from these transactions, representing more than 302
-                    million passenger trips, can provide a unique perspective on
-                    where travelers are going, when they travel and how much
-                    they are paying to get there.
+                    ARC is a premier source of air travel data and insight
+                    through its comprehensive global dataset spanning 9.8
+                    billion passenger trips, 50,000+ travel agencies and 450+
+                    airlines. This data is sourced from ARC’s role facilitating
+                    and settling air ticket transactions between travel agencies
+                    and airlines. In addition to analysis on destination and
+                    purchasing trends, our experts can speak to the issues and
+                    opportunities within air travel distribution, payments and
+                    financial solutions.
                   </p>
-                  <a href="https://www2.arccorp.com/about-us/newsroom" className="ctaBtn">
+                  <a
+                    href="https://www2.arccorp.com/about-us/newsroom"
+                    className="ctaBtn"
+                  >
                     Visit Our Newsroom
                   </a>
                 </div>
@@ -38,11 +43,11 @@ class App extends Component {
             </div>
           </div>
         </div>
-        <div className="container" >
+        <div className="container">
           <div className="row">
             <div className="col-lg-12">
               <h2>Interested in Speaking to ARC for a Story?</h2>
-              <p>
+              <p className="mb-0" style={{ fontSize: "18px" }}>
                 If you need timely data or expert analysis on air travel, we're
                 here to help.
               </p>
@@ -62,7 +67,10 @@ class App extends Component {
           </div>
           <div className="row">
             <div className="col-lg-6">
-              <a href="https://www2.arccorp.com/globalassets/styleguide/arc-logo-suite.zip" className="arc-media-kit-link">
+              <a
+                href="https://www2.arccorp.com/globalassets/styleguide/arc-logo-suite.zip"
+                className="arc-media-kit-link"
+              >
                 <div>Logos</div>{" "}
                 <img
                   src="https://www2.arccorp.com/globalassets/about-us/newsroom/media-kit/file_download---material.png?1"
@@ -71,7 +79,10 @@ class App extends Component {
               </a>
             </div>
             <div className="col-lg-6">
-              <a href="https://www2.arccorp.com/articles-trends/the-latest/coronavirus" className="arc-media-kit-link">
+              <a
+                href="https://www2.arccorp.com/articles-trends/the-latest/coronavirus"
+                className="arc-media-kit-link"
+              >
                 <div>Weekly Segment Data</div>{" "}
                 <img
                   src="https://www2.arccorp.com/globalassets/about-us/newsroom/media-kit/new-page.png?1"
@@ -80,7 +91,10 @@ class App extends Component {
               </a>
             </div>
             <div className="col-lg-6">
-              <a href="https://www2.arccorp.com/styleguide/visual-identity#typography" className="arc-media-kit-link">
+              <a
+                href="https://www2.arccorp.com/styleguide/visual-identity#typography"
+                className="arc-media-kit-link"
+              >
                 <div>Typography and Colors</div>{" "}
                 <img
                   src="https://www2.arccorp.com/globalassets/about-us/newsroom/media-kit/new-page.png?1"
@@ -89,7 +103,10 @@ class App extends Component {
               </a>
             </div>
             <div className="col-lg-6">
-              <a href="https://www2.arccorp.com/articles-trends/sales-statistics/?utm_source=Global_Navigation" className="arc-media-kit-link">
+              <a
+                href="https://www2.arccorp.com/articles-trends/sales-statistics/?utm_source=Global_Navigation"
+                className="arc-media-kit-link"
+              >
                 <div>Monthly Sales Data</div>{" "}
                 <img
                   src="https://www2.arccorp.com/globalassets/about-us/newsroom/media-kit/new-page.png?1"
@@ -98,7 +115,10 @@ class App extends Component {
               </a>
             </div>
             <div className="col-lg-6">
-              <a href="https://www2.arccorp.com/about-us/leadership-governance" className="arc-media-kit-link">
+              <a
+                href="https://www2.arccorp.com/about-us/leadership-governance"
+                className="arc-media-kit-link"
+              >
                 <div>Leadership and Governance</div>{" "}
                 <img
                   src="https://www2.arccorp.com/globalassets/about-us/newsroom/media-kit/new-page.png?1"
@@ -107,7 +127,10 @@ class App extends Component {
               </a>
             </div>
             <div className="col-lg-6">
-              <a href="https://www2.arccorp.com/globalassets/arcmarks/accreditation-logos.zip" className="arc-media-kit-link">
+              <a
+                href="https://www2.arccorp.com/globalassets/arcmarks/accreditation-logos.zip"
+                className="arc-media-kit-link"
+              >
                 <div>Accreditation Logos</div>{" "}
                 <img
                   src="https://www2.arccorp.com/globalassets/about-us/newsroom/media-kit/file_download---material.png?1"
